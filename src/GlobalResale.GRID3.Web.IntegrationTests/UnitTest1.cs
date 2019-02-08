ab57b380ee3a4bb60d554cdd7d28e7f20fcb790e
edsimpat <eduardo@simpatsolutions.com>
2019-02-07 19:25:40 -0600

    Add Web testing projects to the solution

---
+++ src/GlobalResale.GRID3.Web.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GlobalResale.GRID3.Web.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
